6cb8be640b10215647275d3c408c3514f401b8e5
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-13 10:44:21 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (drivers, name) => {
+
+    const result = drivers.filter((x => x.toLowerCase().includes(name.toLowerCase())));
+    
+    return result
+}
+
+const fuzzyMatch = (drivers, name) => {
+
+    const result = drivers.filter((x => x.toLowerCase().indexOf(name.toLowerCase()) == 0));
+
+    return result
+} 
+
+const matchName = (array,name ) => {
+
+    const result = array.filter(x => x.name == name)
+
+    return result
+}
